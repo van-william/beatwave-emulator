7f46eed06de2192f49dadb14317036d1ef352e48
William VanBuskirk <william.n.vanbuskirk@gmail.com>
2025-03-27 22:04:41 -0500

    updated BPM

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,3 @@
-
 import { DrumSound } from "../types";
 
 export const TOTAL_STEPS = 16;
@@ -9,84 +8,84 @@ export const DRUM_SOUNDS: DrumSound[] = [
     id: "kick",
     name: "Kick Drum",
     shortName: "BD",
-    soundFile: "kick.mp3",
+    soundFile: "808/kick.mp3",
     color: "bg-tr808-orange"
   },
   {
     id: "snare",
     name: "Snare Drum",
     shortName: "SD",
-    soundFile: "snare.mp3",
+    soundFile: "808/snare.mp3",
     color: "bg-tr808-orange-light"
   },
   {
     id: "clap",
     name: "Clap",
     shortName: "CP",
-    soundFile: "clap.mp3",
+    soundFile: "808/clap.mp3",
     color: "bg-tr808-orange-light"
   },
   {
     id: "rim",
     name: "Rim Shot",
     shortName: "RS",
-    soundFile: "rimshot.mp3",
+    soundFile: "808/rimshot.mp3",
     color: "bg-tr808-orange-light"
   },
   {
     id: "tom-low",
     name: "Low Tom",
     shortName: "LT",
-    soundFile: "tom-low.mp3",
+    soundFile: "808/tom-low.mp3",
     color: "bg-tr808-amber"
   },
   {
     id: "tom-mid",
     name: "Mid Tom",
     shortName: "MT",
-    soundFile: "tom-mid.mp3",
+    soundFile: "808/tom-mid.mp3",
     color: "bg-tr808-amber"
   },
   {
     id: "tom-hi",
     name: "High Tom",
     shortName: "HT",
-    soundFile: "tom-high.mp3",
+    soundFile: "808/tom-high.mp3",
     color: "bg-tr808-amber"
   },
   {
     id: "ch",
     name: "Closed Hat",
     shortName: "CH",
-    soundFile: "hihat-closed.mp3",
+    soundFile: "808/hihat-closed.mp3",
     color: "bg-tr808-amber"
   },
   {
     id: "oh",
     name: "Open Hat",
     shortName: "OH",
-    soundFile: "hihat-open.mp3",
+    soundFile: "808/hihat-open.mp3",
     color: "bg-tr808-cream"
   },
   {
     id: "cymbal",
     name: "Cymbal",
     shortName: "CY",
-    soundFile: "cymbal.mp3",
+    soundFile: "808/cymbal.mp3",
     color: "bg-tr808-cream"
   },
   {
     id: "cowbell",
     name: "Cowbell",
     shortName: "CB",
-    soundFile: "cowbell.mp3",
+    soundFile: "808/cowbell.mp3",
     color: "bg-tr808-cream"
   },
   {
     id: "clave",
     name: "Clave",
     shortName: "CL",
-    soundFile: "clave.mp3",
+    soundFile: "808/clave.mp3",
     color: "bg-tr808-cream"
   }
 ];
